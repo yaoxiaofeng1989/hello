91dd363cafcab35b66b9d3b99e8f5a18007a12f6
yaoxiaofeng <yaoxiaofeng007@163.com>
2014-08-22 16:06:58 +0800

    我提交了一个PHP文件

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+
+echo urldecode("{%22typeName%22%3A%22ALL%22%2C%22platform%22%3A%224%22%2C%22start%22%3A60%2C%22isProduct%22%3A%220%22%2C%22authInfo%22%3A{%22uid%22%3A%2239002516%22%2C%22sessionCode%22%3A%220253219407d118edbe3ab7a69366edb5%22}%2C%22locale%22%3A%22zh_CN%22%2C%22type%22%3A%22default%22%2C%22end%22%3A89%2C%22typeID%22%3A%220%22}");
+echo '--------------------------------------<br>';
+echo urldecode('{"typeName":"ALL","platform":"4","start":0,"isProduct":"0","authInfo":{"uid":"60156765","sessionCode":"0395eb5dfee36eb63141db31bf25549b"},"locale":"zh_CN","type":"default","end":29,"typeID":"0"}');
+?>
